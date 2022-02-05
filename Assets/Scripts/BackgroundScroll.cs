18b34060e173589eccaaa4625049650144833f7d
Presli00 <ice_rock@abv.bg>
2022-02-05 14:23:34 +0200

    moving BG elements are ready

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -32,14 +32,15 @@ public class BackgroundScroll : MonoBehaviour
             GameObject s = Instantiate(obj) as GameObject;
             s.transform.position = new Vector3(Random.Range(-bounds.x, bounds.x), bounds.y * 2, Random.Range(10, 110));
             rb = s.GetComponent<Rigidbody2D>();
+            string n = obj.name;
             SpriteRenderer sr = s.GetComponent<SpriteRenderer>();
             float parallaxSpeed = 1 - Mathf.Abs(transform.position.z / s.transform.position.z);
             float alterSize = Mathf.Abs(transform.position.z - s.transform.position.z);
-            if (s.name.Equals("Galaxy"))
+            if (obj.name == "Galaxy")
             {
-                rb.velocity = new Vector2(0f, -planetSpeed);
+                rb.velocity = new Vector2(0f, parallaxSpeed);
                 sr.size = new Vector2(5 + sr.size.x, 5 + sr.size.y);
-                sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0.5f);
+                sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0.7f);
             }
             else
             {
