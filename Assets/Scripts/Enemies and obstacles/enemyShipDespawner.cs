025cf50f5b3fdcd62fc0ee5315bdc2bd49b556db
Presli00 <ice_rock@abv.bg>
2022-07-23 19:46:28 +0300

    Enemy AI can target the player.

--- Assets/Scripts/Enemies and obstacles/enemyShipDespawner.cs
+++ Assets/Scripts/Enemies and obstacles/enemyShipDespawner.cs
@@ -6,11 +6,13 @@ public class enemyShipDespawner : MonoBehaviour
 {
     private Vector2 bounds;
     private Camera mainCamera;
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
         bounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -19,6 +21,9 @@ public class enemyShipDespawner : MonoBehaviour
         if (Mathf.Round(transform.position.y).Equals(Mathf.Round(bounds.y / 1.5f)))
         {
             this.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Vector3 dir = player.position - transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, angle-90), 2 * Time.deltaTime);
         }
         if (LineRendererScript.hit)
         {
